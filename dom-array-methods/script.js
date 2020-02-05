2c2f0f8e0662ea2cf08b587b97f940ede8c537db
Blazekla <cristlui4@gmail.com>
2020-02-05 08:45:33 -0600

    added code to fetch api for dom array methods project

---
+++ dom-array-methods/script.js
@@ -0,0 +1,28 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+//Fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  addData(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+}
