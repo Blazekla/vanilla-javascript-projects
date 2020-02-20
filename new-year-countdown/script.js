acd9e65b4d9c410be8626bf56d4c9a9db07886c4
Blazekla <cristlui4@gmail.com>
2020-02-20 08:51:03 -0600

    added js code to add countdown timer and spinner to the countdown project

--- new-year-countdown/script.js
+++ new-year-countdown/script.js
@@ -3,8 +3,12 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
+//set background year
+year.innerHTML = currentYear + 1;
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
@@ -12,6 +16,7 @@ console.log(currentYear);
 console.log(newYearTime);
 console.log(newYearTime - currentYear);
 
+//Update timer
 function countdownTimer() {
   const currentTime = new Date();
   const remainingTime = newYearTime - currentTime;
@@ -20,7 +25,7 @@ function countdownTimer() {
   const h = Math.floor(remainingTime / 1000 / 60 / 60) % 24;
   const m = Math.floor(remainingTime / 1000 / 60) % 60;
   const s = Math.floor(remainingTime / 1000) % 60;
-  console.log(h);
+
   // Add values to DOM
   days.innerHTML = d;
   hours.innerHTML = h < 10 ? "0" + h : h;
@@ -28,4 +33,13 @@ function countdownTimer() {
   seconds.innerHTML = s < 10 ? "0" + s : s;
 }
 
+//Show Spinner
+setTimeout(() => {
+  loading.remove();
+  //It isn't recommended to use setAttribute for styling, user properties of the style object instead
+  // countdown.setAttribute("style", "display:flex");
+  countdown.style.display = "flex";
+}, 1000);
+
+//Run functions every second
 setInterval(countdownTimer, 1000);
