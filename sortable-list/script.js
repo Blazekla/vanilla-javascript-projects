8180802733bf99759dc7ac6957aaae6fe7cf688b
Luis Cristo <cristlui4@gmail.com>
2020-02-25 19:48:44 -0600

    added more js code to the sortable list project

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -24,19 +24,22 @@ createList();
 //Insert list items into DOM
 
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("data-index", index);
+  [...richestPeople]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .forEach((person, index) => {
+      console.log(person);
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
         <i class="fas fa-grip-lines"></i>
         </div>`;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.appendChild(listItem);
-  });
+      draggable_list.appendChild(listItem);
+    });
 }
