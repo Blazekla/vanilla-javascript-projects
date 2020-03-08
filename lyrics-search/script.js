0d3fea14004bf12344716cf6b3e324f0a2f34170
Blazekla <cristlui4@gmail.com>
2020-03-08 17:36:06 -0500

    added code to get lyrics for the lyrics project

--- lyrics-search/script.js
+++ lyrics-search/script.js
@@ -64,6 +64,16 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+//Get Lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2><span>${lyrics}</span>`;
+  more.innerHTML = "";
+}
+
 //Event Listener
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -73,5 +83,17 @@ form.addEventListener("submit", e => {
     alert("Please type in a search term");
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+//Get Lyrics button click
+result.addEventListener("click", e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
   }
 });
