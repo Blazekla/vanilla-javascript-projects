2849e75da1493477d2478c3e924eac24709d5fb7
Blazekla <cristlui4@gmail.com>
2020-03-08 17:19:11 -0500

    added pagination code to the lyrics project

--- lyrics-search/script.js
+++ lyrics-search/script.js
@@ -10,11 +10,59 @@ async function searchSongs(term) {
   const result = await fetch(`${apiURL}/suggest/${term}`);
   const data = await result.json();
 
+  console.log(data);
   showData(data);
 }
 
 //Show song and artist in DOM
-function showData(data) {}
+function showData(data) {
+  // let output = "";
+  // data.data.forEach(song => {
+  //   console.log(song);
+
+  //   output += `<li>
+  //   <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //   <button class="btn" data-artist="${song.artist.name}"
+  //    data-songtitle="${song.title}">Get Lyrics</button>
+  //   </button></li>`;
+  // });
+  // result.innerHTML = `<ul class="songs">${output}</ul>`;
+
+  result.innerHTML = `<ul class="songs">${data.data
+    .map(
+      song => `<li>
+<span><strong>${song.artist.name}</strong> - ${song.title}</span>
+<button class="btn" data-artist="${song.artist.name}"
+ data-songtitle="${song.title}">Get Lyrics</button>
+</button></li>`
+    )
+    .join("")}</ul>`;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
+      `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+//Get prev and next songs
+async function getMoreSongs(url) {
+  const result = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await result.json();
+
+  showData(data);
+}
 
 //Event Listener
 form.addEventListener("submit", e => {
